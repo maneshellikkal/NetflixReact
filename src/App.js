f292ee6d18f6b701cce8f5f6d5acf66b23962a76
maneshellikkal <maneshelk@gmail.com>
2021-10-26 01:43:43 -0700

    add new urls

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import Banner from './component/Banner/Banner';
 import NavBar from './component/NavBar/NavBar';
 import RowPost from './component/RowPost/RowPost';
+import{action,originals,romance,comedy} from './urls'
 
 
 
@@ -12,7 +13,10 @@ function App() {
     <div className="App">
       <Banner/>
       <NavBar/>
-      <RowPost/>
+      <RowPost url={originals} title="Netflix Orginals"/>
+      <RowPost url={action} title="Action" isSmall/>
+      <RowPost url={romance} title="Romance" isSmall/>
+      <RowPost url={comedy} title="Comedy" isSmall/>
         
         
     </div>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,3 @@
 export const baseUrl ='https://api.themoviedb.org/3';
-export const API_KEY =''
+export const API_KEY ='26124fca5078ff03a493a35df7fc1a7f';
+export const imageUrl='https://image.tmdb.org/t/p/original'
